cc7fdb71f7cb6e3511af7812cffb2fc2dd189572
SanZangana <San9@live.se>
2022-11-25 12:05:41 +0000

    Add js src to html documents

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleMenu = document.getElementsByClassName('toggle-menu')[0]
-const nbLink = document.getElementsByClassName('nb-link')[0]
-
-toggleMenu.addEventListener('click', () => {
-  nbLink.classList.toggle('active')
-})
